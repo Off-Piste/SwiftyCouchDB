0599c9f4c9052b2379be067c3c474139b19ab702
harrytwright <haroldtomwright@gmail.com>
2017-08-10 19:34:24 +0400

    tests

--- install_swiftlint.sh
+++ install_swiftlint.sh
@@ -6,18 +6,11 @@
 
 set -e
 
-SWIFTLINT_PKG_PATH="/tmp/SwiftLint.pkg"
-SWIFTLINT_PKG_URL="https://github.com/realm/SwiftLint/releases/download/0.9.1/SwiftLint.pkg"
-
-wget --output-document=$SWIFTLINT_PKG_PATH $SWIFTLINT_PKG_URL
-
-if [ -f $SWIFTLINT_PKG_PATH ]; then
-  echo "SwiftLint package exists! Installing it..."
-  sudo installer -pkg $SWIFTLINT_PKG_PATH -target /
+if which swiftlint >/dev/null; then
+    brew upgrade swiftlint
+    swiftlint
 else
-  echo "SwiftLint package doesn't exist. Compiling from source..." &&
-  git clone https://github.com/realm/SwiftLint.git /tmp/SwiftLint &&
-  cd /tmp/SwiftLint &&
-  git submodule update --init --recursive &&
-  sudo make install
+    echo "SwiftLint not installed, downloading from https://github.com/realm/SwiftLint"
+    brew install swiftlint
+    swiftlint
 fi
