f4cf17b8c2cb90b98388b1e3d871fe7f243091e5
harrytwright <haroldtomwright@gmail.com>
2017-08-11 14:08:51 +0400

    Docs Added

--- install_swiftlint.sh
+++ install_swiftlint.sh
@@ -8,56 +8,74 @@ set -o pipefail
 set -e
 
 source_root="$(dirname "$0")"
+source="$0"
 
-function downloadSwiftlint {
-    if which swiftlint >/dev/null; then
-        echo "SwiftLint is installed"
-        brew outdated swiftlint || brew upgrade swiftlint
-    else
-        echo "SwiftLint not installed, downloading via homebrew"
-        brew install swiftlint
-    fi
+function usage {
+  echo "$source [tests-macos, swiftlint, jazzy, before-swiftlint, before-tests-macos]"
+  exit 0
 }
 
-function runSwiftlint {
-    echo "Running SwiftLint"
-    swiftlint
+function xcode {
+  CMD="$@"
+  echo "Building with command:" $CMD
+  eval "$CMD"
 }
 
-function runTests {
+#######################################
+#             Helper Code             #
+#######################################
+
+function pre_xcode {
+  gem install xcpretty --no-rdoc --no-ri --no-document --quiet
+  gem install xcpretty-travis-formatter --no-rdoc --no-ri --no-document --quiet
+
+  echo "Building Swift"
+  swift build
+
+  echo "Generating Package"
+  swift package generate-xcodeproj
+}
+
+function build_for_swiftlint {
+  if which swiftlint >/dev/null; then
+      echo "SwiftLint is installed"
+      brew outdated swiftlint || brew upgrade swiftlint
+  else
+      echo "SwiftLint not installed, downloading via homebrew"
+      brew install swiftlint
+  fi
+}
+
+#######################################
+#             Running Code            #
+#######################################
+
+function _swiftlint {
+  echo "Running SwiftLint"
+  swiftlint
+}
+
+function run_tests {
   XCODE_TESTS_PARAMS="-project $source_root/SwiftyCouchDB.xcodeproj -scheme SwiftyCouchDBTests"
 
   echo "Running Tests"
   xcode "xcodebuild clean build test $XCODE_TESTS_PARAMS | xcpretty -f `xcpretty-travis-formatter`"
 }
 
-function downloadHelperCode {
-  gem install xcpretty --no-rdoc --no-ri --no-document --quiet;  gem install xcpretty-travis-formatter --no-rdoc --no-ri --no-document --quiet
+function _jazzy {
+  jazzy -x -project,SwiftyCouchDB.xcodeproj,-scheme,SwiftyCouchDB --hide-documentation-coverage
+  rm -R ./build
 }
 
-function xcode {
-  mkdir -p ./build/DerivedData
-  CMD="$@ -IDECustomDerivedDataLocation=build/DerivedData"
-  echo "Building with command:" $CMD
-  eval "$CMD"
-}
-
-function createXcodeMacOS {
-    echo "Building Swift"
-    swift build
-
-    echo "Generating Package"
-    swift package generate-xcodeproj
-}
-
-function createXcodeLinux {
-    swift build
-}
+#######################################
+#           Parse Argument            #
+#######################################
 
 case $1 in
-    swiftlint) downloadSwiftlint; runSwiftlint ;;
-    test-server) runTests ;;
-    pre-build-mac) downloadHelperCode; createXcodeMacOS ;;
-    pre-build-linux) downloadHelperCode; createXcodeLinux ;;
-    *) echo "Invalid Request"; exit 1 ;;
+    swiftlint) _swiftlint ;;
+    tests-macos) run_tests ;;
+    jazzy) _jazzy ;;
+    before-tests-macos) pre_xcode ;;
+    before-swiftlint) build_for_swiftlint ;;
+    *) usage ;;
 esac
