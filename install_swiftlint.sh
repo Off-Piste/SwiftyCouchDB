aa022f084a695838a8359b400cd5a3deed3c8231
harrytwright <haroldtomwright@gmail.com>
2017-08-11 11:06:20 +0400

    Tests 2

--- install_swiftlint.sh
+++ install_swiftlint.sh
@@ -4,13 +4,40 @@
 # Tries to get the precompiled .pkg file from Github, but if that
 # fails just recompiles from source.
 
+set -o pipefail
 set -e
 
-if which swiftlint >/dev/null; then
-    brew upgrade swiftlint
+source_root="$(dirname "$0")"
+
+# XCPRETTY_PARAMS="-f `xcpretty-travis-formatter`"
+# XCODE_TESTS_PARAMS="-project $source_root/SwiftyCouchDB.xcodeproj -scheme SwiftyCouchDBTests"
+
+function downloadSwiftlint {
+    if which swiftlint >/dev/null; then
+        echo "SwiftLint is installed"
+        brew outdated swiftlint || brew upgrade swiftlint
+    else
+        echo "SwiftLint not installed, downloading via homebrew"
+        brew install swiftlint
+    fi
+}
+
+function runSwiftlint {
+    echo "Running SwiftLint"
     swiftlint
-else
-    echo "SwiftLint not installed, downloading from https://github.com/realm/SwiftLint"
-    brew install swiftlint
-    swiftlint
-fi
+}
+
+function runTests {
+    echo "Running Tests"
+    xcodebuild clean build test -project $source_root/SwiftyCouchDB.xcodeproj -scheme SwiftyCouchDBTests | xcpretty -f `xcpretty-travis-formatter`
+}
+
+case $1 in
+    swiftlint)
+        downloadSwiftlint
+        runSwiftlint
+        ;;
+    test-server)
+        runTests
+        ;;
+esac
